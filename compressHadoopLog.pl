b0f43f23ffde388890ed18e7864e8c10ce0c121d
Tran Dinh Hung <hungtd@gmail.com>
2013-04-15 22:19:34 +0700

    commit

--- compressHadoopLog.pl
+++ compressHadoopLog.pl
@@ -7,4 +7,8 @@ use constant {
 my $hadoop_log_dir = "/data2/mgsys/log/hadoop_log_gw";
 my $file_name = "hadoop.log";
 
+unless (-e $hadoop_log_dir."/".$file_name) {
+	print "Hadoop log file doesn't exist!";
+}
+
 
