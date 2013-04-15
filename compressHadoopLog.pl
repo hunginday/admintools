7ae0ac91cebd9622161a290b97a05976b4547f84
Tran Dinh Hung <hungtd@gmail.com>
2013-04-15 23:37:08 +0700

    commit

--- compressHadoopLog.pl
+++ compressHadoopLog.pl
@@ -24,8 +24,8 @@ unless ($filesize >= FILE_SIZE) {
 	print "Hadoop log file is smaller than ".FILE_SIZE." ($filesize)";
 }
 
-($second, $minute, $hour, $dayOfMonth, $month, $yearOffset, $dayOfWeek, $dayOfYear, $daylightSavings) = localtime();
-my $postfix = sprintf("%04d%02d%02d%02d%02d%02d", $yearOffset+1900, $month, $dayOfMonth, $hour, $minute, $second);
+($second, $minute, $hour, $dayOfMonth, $month, $yearOffset, $dayOfWeek, $dayOfYear, $daylightSavings) = localtime(time);
+my $postfix = sprintf("%04d%02d%02d%02d%02d%02d", $yearOffset+1900, $month+1, $dayOfMonth, $hour, $minute, $second);
 
 print "$postfix\n";
 
