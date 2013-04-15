d9deb7f82c55b9a0fc4ced69e873b85c035e7828
Tran Dinh Hung <hungtd@gmail.com>
2013-04-15 23:56:18 +0700

    commit

--- compressHadoopLog.pl
+++ compressHadoopLog.pl
@@ -3,14 +3,14 @@ use strict;
 use warnings;
 
 use constant {
-	FILE_SIZE => 5 #Megabytes
+	FILE_SIZE => 5*1024 #Megabytes
 };
 
 #my $hadoop_log_dir = "/data2/mgsys/log/hadoop_log_gw";
 #my $file_name = "hadoop.log";
-
 my $hadoop_log_dir = "/home/mgsys/admintools";
 my $file_name = "file5M.log";
+
 my $file = $hadoop_log_dir."/".$file_name;
 
 unless (-e $file) {
@@ -22,7 +22,7 @@ my $filesize = -s $file;
 $filesize = $filesize / (1024*1024);
 
 unless ($filesize >= FILE_SIZE) {
-	print "Hadoop log file is smaller than ".FILE_SIZE." ($filesize)";
+	print "Hadoop log file is smaller than ".FILE_SIZE." ($filesize)\n";
 	exit 2;
 }
 
