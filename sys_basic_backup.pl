bee6b06fbdd866f369980e7e7b6429133467cc4c
HungTD <hungtd@gmail.com>
2013-04-23 16:03:18 +0700

    ok

--- sys_basic_backup.pl
+++ sys_basic_backup.pl
@@ -2,7 +2,23 @@
 use strict;
 use warnings;
 
-use constant {
-	FILE_SIZE => 20*1024 #Megabytes
-};
+my $log_dir = "/monitoring";
+my $file_name = "sysbasic.log";
+my $file = $log_dir."/".$file_name;
 
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+my $postfix = sprintf("%04d%02d%02d%02d%02d%02d", $year+1900, $mon+1, $mday, $hour, $min, $sec);
+
+my $new_file = $file.".$postfix";
+
+open (log_file, '>>data.txt');
+print log_file "Bob\n";
+close (log_file); 
+
+
+
+
+
+
+
+exit 0;
