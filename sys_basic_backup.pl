e450d966bb76c775caaed8c965d3184cb522f592
HungTD <hungtd@gmail.com>
2013-04-25 13:04:31 +0700

    ok

--- sys_basic_backup.pl
+++ sys_basic_backup.pl
@@ -2,23 +2,33 @@
 use strict;
 use warnings;
 
-my $log_dir = "/monitoring";
-my $file_name = "sysbasic.log";
-my $file = $log_dir."/".$file_name;
-
+my $log_dir = "/monitoring/sys_basic";
+my $name = "sysbasic.log";
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 my $postfix = sprintf("%04d%02d%02d%02d%02d%02d", $year+1900, $mon+1, $mday, $hour, $min, $sec);
+my $file_name = $log_dir."/".$name.".$postfix";
 
-my $new_file = $file.".$postfix";
+open (log_file, '>', $file_name);
 
-open (log_file, '>>data.txt');
-print log_file "Bob\n";
+echo_title("route -n");
+my $rs = `route -n`; 
+print log_file $rs;
+
+echo_title("crontab -uroot -l");
+$rs = `crontab -uroot -l`; 
+print log_file $rs;
+
+echo_title("crontab -umgsys -l");
+$rs = `crontab -umgsys -l`; 
+print log_file $rs;
+
 close (log_file); 
-
-
-
-
-
-
-
 exit 0;
+
+sub echo_title
+{
+	my ($text) = @_;
+	print log_file "====================\n";
+	print log_file "$text\n";
+	print log_file "====================\n";
+}
