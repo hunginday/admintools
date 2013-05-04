4543942d6106a069510501195b0a9a94bf01c685
HungTD <hungtd@gmail.com>
2013-05-04 13:55:55 +0700

    ok

--- clean_mgsys_log.pl
+++ clean_mgsys_log.pl
@@ -9,30 +9,30 @@ my $month_time = timelocal(0,0,0, (localtime(time() - 30 * 24 * 3600))[3,4,5]);
 my @dir = ('/data2/mgsys/log');
 
 for my $dir (@dir) {
-        opendir(DIR, $dir);
-        while (my $file = readdir(DIR)) {
-                next if ($file =~ /^\./);
+	opendir(DIR, $dir);
+	while (my $file = readdir(DIR)) {
+		next if ($file =~ /^\./);
 		print ("file=$file\n");
 		my $path = "$dir/$file";
-		my $time;                
-
+		my $time;
 		if ($file =~ /\.(\d\d\d\d)(\d\d)(\d\d)$/) {
-        	        eval {
-                	        $time = timelocal(0,0,0,$3,$2-1,$1);
-                	};
+			eval {
+				$time = timelocal(0,0,0,$3,$2-1,$1);
+			};
 			next if ($@);
-        	        next if ($time >= $month_time);
-	              	print "gzip -9 file: $path\n";
+			next if ($time >= $month_time);
+			print "gzip -9 file: $path\n";
 			system ("gzip -9 $path");
 		} elsif ($file =~ /\.(\d\d\d\d)(\d\d)(\d\d)\.gz/) {
 			eval {
-                                $time = timelocal(0,0,0,$3,$2-1,$1);
-                        };
+				$time = timelocal(0,0,0,$3,$2-1,$1);
+			};
 			next if ($@);
-                        next if ($time >= $old_time);
+			next if ($time >= $old_time);
 			print "delete gzip file: $path\n";
 			unlink($path);
 		}
-        }
-        closedir(DIR);
+	}
+	closedir(DIR);
 }
+
