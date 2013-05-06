aefa6f337007a07c223d372cc694378a7dcea8cf
HungTD <hungtd@gmail.com>
2013-05-06 16:20:22 +0700

    ok

--- compressHadoopLog.pl
+++ compressHadoopLog.pl
@@ -3,13 +3,11 @@ use strict;
 use warnings;
 
 use constant {
-	FILE_SIZE => 20*1024 #Megabytes
+	FILE_SIZE => 5*1024 #Megabytes
 };
 
 my $hadoop_log_dir = "/data2/mgsys/log/hadoop_log_gw";
 my $file_name = "hadoop.log";
-#my $hadoop_log_dir = "/home/mgsys/admintools";
-#my $file_name = "file5M.log";
 
 my $file = $hadoop_log_dir."/".$file_name;
 
