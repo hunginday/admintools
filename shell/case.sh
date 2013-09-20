d24e7127c72b776452a41747663e6ff3e6edd7d8
HungTD <hungtd@gmail.com>
2013-09-20 14:34:11 +0700

    ok

--- shell/case.sh
+++ shell/case.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
-echo "Usage: $0 start|stop"
+echo "Usage: $0 start|stop|search"
+
+NOW=$(date +"%d-%m-%Y")
+HOME=.
+
 case "$1" in
     start)
         echo "start command!"
+        #export -p
+        read -t 3 -p "Enter your name : " name
+        echo "Hi, $name. Let us be friends!"
+        echo "Dumping sql at $NOW"
+        ;;
+    search)
+        find $HOME -name \*.sh
         ;;
     stop)
         echo "stop command!"
