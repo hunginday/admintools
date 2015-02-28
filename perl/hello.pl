21ea80ff211b6e48f6bcebbb16a70204c05f67cb
Hung Tran <hung.tran@dena.com>
2015-03-01 01:25:05 +0700

    ok

--- perl/hello.pl
+++ perl/hello.pl
@@ -17,3 +17,29 @@ print "test4: ".$scalarRef."\n";
 print "test5: ".${$scalarRef}."\n";
 print "test6: ".$$scalarRef."\n";
 
+# Braces denote an anonymous hash
+# Square brackets denote an anonymous array
+
+my %account = (
+    "number" => "31415926",
+    "opened" => "3000-01-01",
+    "owners" => [
+        {
+            "name" => "Philip Fry",
+            "DOB"  => "1974-08-06",
+        },
+        {
+            "name" => "Hubert Farnsworth",
+            "DOB"  => "2841-04-09",
+        },
+    ],
+);
+
+my @array1 = (1, 2, 3, 4, 5);
+my @array2 = [1, 2, 3, 4, 5];
+
+print "array1: ".Dumper(@array1)."\n";
+print "array2: ".Dumper($array2)."\n";
+
+
+
