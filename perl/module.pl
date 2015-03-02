cfe30fac5834b9ef991ab57180cf511d9d606c88
Hung Tran <hung.tran@dena.com>
2015-03-02 14:43:58 +0700

    ok

--- perl/module.pl
+++ perl/module.pl
@@ -4,3 +4,7 @@ use warnings;
 require Demo::StringUtils;
 
 print zombify("i want brains"); # "r wrnt brrrns"
+
+require Demo::StringUtils2;
+
+print Demo::StringUtils2::zombify("i want brains"); # "r wrnt brrrns"
