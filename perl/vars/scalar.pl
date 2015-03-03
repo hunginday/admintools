d6cd7bc2cd45ea7ecedeff4ca6cf3cb13b80ffec
Hung Tran <hung.tran@dena.com>
2015-03-03 16:13:43 +0700

    ok

--- perl/vars/scalar.pl
+++ perl/vars/scalar.pl
@@ -28,8 +28,8 @@ my $ary = \@myarray; # reference to a named array
 my $hsh = \%myhash; # reference to a named hash
 my $sub = \&mysub; # reference to a named subroutine
 
-my $ary = [1,2,3,4,5]; # reference to an unnamed array
-my $hsh = {Na => 19, Cl => 35}; # reference to an unnamed hash
-my $sub = sub { print $state }; # reference to an unnamed subroutine
+my $ary_ref = [1,2,3,4,5]; # reference to an unnamed array
+my $hsh_ref = {Na => 19, Cl => 35}; # reference to an unnamed hash
+my $sub_ref = sub { print $state }; # reference to an unnamed subroutine
 
 my $fido = Camel–>new("Amelia"); # reference to an object
