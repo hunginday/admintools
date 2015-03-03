aa88dbe68c04023766d119df293e7e517ce94eb2
Hung Tran <hung.tran@dena.com>
2015-03-03 16:12:44 +0700

    ok

--- perl/vars/scalar.pl
+++ perl/vars/scalar.pl
@@ -15,9 +15,19 @@ my $salsa = $moles * $avocados; # a gastrochemical expression
 my $exit = system("vi $file"); # numeric status of a command
 my $cwd = `pwd`; # string output from a command
 
+
+my $state   = "hello scalars"; 
+my @myarray = ("hello", "scalars");
+my %myhash  = ("hello", "scalars");
+
+sub mysub {
+    return 1;
+}
+
 my $ary = \@myarray; # reference to a named array
 my $hsh = \%myhash; # reference to a named hash
 my $sub = \&mysub; # reference to a named subroutine
+
 my $ary = [1,2,3,4,5]; # reference to an unnamed array
 my $hsh = {Na => 19, Cl => 35}; # reference to an unnamed hash
 my $sub = sub { print $state }; # reference to an unnamed subroutine
