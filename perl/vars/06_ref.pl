7de91ba30c762524f24276c72930bf1775899c5b
Hung Tran <hung.tran@dena.com>
2015-03-05 17:34:21 +0700

    ok

--- perl/vars/06_ref.pl
+++ perl/vars/06_ref.pl
@@ -30,7 +30,6 @@ my $sref = sub {
     return $a + $b;
 };
 
-
 print $aref->[3]."\n";      # an array dereference
 print $href->{"Sun"}."\n";  # a hash dereference
 print $sref->(1,2)."\n";    # a subroutine dereference
@@ -38,6 +37,33 @@ print $sref->(1,2)."\n";    # a subroutine dereference
 
 # a reference to an anonymous array
 my $arrayref = [1, 2, ["a", "b", "c", "d"]];
+print '$arrayref->[2][1] = '.$arrayref->[2][1]."\n";
+
+# hash of arrays
+my $table1 = {
+    "john" => [ 47, "brown", 186 ],
+    "mary" => [ 23, "hazel", 128 ],
+    "bill" => [ 35, "blue", 157 ],
+};
+
+# hash of hashes
+my $table2 = {
+    "john" => { age => 47,
+        eyes => "brown",
+        weight => 186,
+    },
+    "mary" => { age => 23,
+        eyes => "hazel",
+        weight => 128,
+    },
+    "bill" => { age => 35,
+        eyes => "blue",
+        weight => 157,
+    },
+};
+
+# ref to an anonymous subroutine
+my $coderef = sub { print "Boink!\n" };
 
 
 print "end\n";
