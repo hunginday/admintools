805179ad074ccebd88c38eb6a3be6de849b568df
Hung Tran <hung.tran@dena.com>
2015-03-09 18:15:25 +0700

    ok

--- perl/modules/module_test.pl
+++ perl/modules/module_test.pl
@@ -4,3 +4,9 @@ use Bestiary::OO;
 my $bestiary = Bestiary::OO->new; # class method
 say "Animal is ", $bestiary->camel(),
 " has weight ", $bestiary->weight();
+
+
+use Bestiary qw(camel $weight);
+say "Animal is ", camel(), " has weight $weight";
+
+print "end\n";
