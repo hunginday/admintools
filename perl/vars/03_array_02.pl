4970ef30e787612b81916bca1ceec66350aef138
Hung Tran <hung.tran@dena.com>
2015-03-12 18:34:47 +0700

    ok

--- perl/vars/03_array_02.pl
+++ perl/vars/03_array_02.pl
@@ -13,9 +13,41 @@ pop @array; # @array now has (5, 6). (The 7 is discarded.)
 push(@array, 0); # @array now has (5, 6, 0)
 push @array, 8; # @array now has (5, 6, 0, 8)
 push @array, 1..10; # @array now has those 10 new elements
-my @others = qw/ 9 0 2 1 0 /;
+
+dump @array;
+
+print Dumper(@array)."\n";
+
+my @others = qw/ hanoi hue saigon /;
 push @array, @others; # @array now has those five new elements (19 total)
 
 dump @array;
 
-print "end\n";
+my @rocks = qw{ flintstone slate rubble };
+print "quartz @rocks limestone\n"; # prints five rocks separated by spaces
+
+@rocks = qw/ bedrock slate lava /;
+
+my $rock = "test";
+
+foreach $rock (@rocks) {
+    $rock = "\t$rock"; # put a tab in front of each element of @rocks
+    $rock .= "\n"; # put a newline on the end of each
+}
+print "The rocks are:\n", @rocks; # Each one is indented, on its own line
+
+print "rock is still $rock\n";
+
+foreach (1..10) { # Uses $_ by default
+    print "I can count to $_!\n";
+}
+
+my @rev = qw /hanoi hungyen haiphong/;
+push @rev, qw /3 4 5/;
+
+my @last_rev = reverse @rev;
+print "last_rev @last_rev \n";
+
+print @last_rev;
+
+print "\n\n\n$#array: end\n";
