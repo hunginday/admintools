c969c155695cfcef0a9f277151eb6d8d2c26efd6
Hung Tran <hung.tran@dena.com>
2015-03-16 00:32:44 +0700

    ok

--- perl/vars/12_print.pl
+++ perl/vars/12_print.pl
@@ -5,4 +5,25 @@ use warnings;
 use Data::Dumper;
 use Data::Dump qw(dump); # CPAN module
 
-print (2+3)*4; # Oops!
+#print (2+3)*4; # Oops!
+
+if (@ARGV < 2) {
+    #die "Not enough arguments";
+}
+
+my $noone;
+
+my $hashref = {};
+
+#$hashref->{"ok"} .= "test";
+$hashref->{"ok"} .= $noone;
+
+
+$hashref->{"dino"} = undef;
+
+if (exists $hashref->{"dino"}) {
+    print "this is strange\n";
+}
+
+
+dump $hashref;
