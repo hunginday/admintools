5ed42f36fce88dfb5e5bfe8a7485ec584899ad91
Hung Tran <hung.tran@dena.com>
2015-04-27 00:53:31 +0700

    ok

---
+++ shell/download_album.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ALBUM_URL="http://mp3.zing.vn/album/Ke-O-Mien-Xa-Quang-Le/ZWZAIFOI.html"
+ALBUM_MODE="--compressed"
+
+echo $ALBUM_URL
+
+
--- shell/download_song.sh
+++ shell/download_song.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-
 SONG_URL="http://mp3.zing.vn/bai-hat/Dat-Viet-Buc-Tuong/ZW6FODWZ.html"
 SONG_MODE="--compressed"
 
-
+song_name=`echo $SONG_URL | sed -e 's/.*bai-hat\/\(.*\)\/.*/\1/'`
 song_xml=`curl $SONG_MODE $SONG_URL 2>/dev/null | grep -oE '<embed id.*></embed>' | sed -e 's/.*xmlURL=\(.*\)\&amp;.*/\1/'`
 song_url=`curl $SONG_MODE $song_xml 2>/dev/null | grep -oE '<source>.*</source>' | sed -e 's/.*\[CDATA\[\(.*\)\]\].*/\1/'`
 
-echo "song URL: $song_url"
-
+curl -o "$song_name.mp3" -L $song_url
