e2cf1bc9b029bd6d68569d1432544ae00a737fdc
Hung Tran <hung.tran@dena.com>
2015-04-27 01:48:23 +0700

    ok

--- shell/download_album.sh
+++ shell/download_album.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ALBUM_URL="http://mp3.zing.vn/album/Ke-O-Mien-Xa-Quang-Le/ZWZAIFOI.html"
-ALBUM_MODE="--compressed"
+INPUT_URL="http://mp3.zing.vn/album/Ke-O-Mien-Xa-Quang-Le/ZWZAIFOI.html"
+CURL_MODE="--compressed"
 
-echo $ALBUM_URL
+album_xml_url=`curl $CURL_MODE $INPUT_URL 2>/dev/null | grep -oE '<embed id.*></embed>' | sed -e 's/.*xmlURL=\(.*\)\&amp;.*/\1/'`
 
-
+echo $album_xml_url
--- shell/download_song.sh
+++ shell/download_song.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-SONG_URL="http://mp3.zing.vn/bai-hat/Dat-Viet-Buc-Tuong/ZW6FODWZ.html"
-SONG_MODE="--compressed"
+INPUT_URL="http://mp3.zing.vn/bai-hat/Dat-Viet-Buc-Tuong/ZW6FODWZ.html"
+CURL_MODE="--compressed"
 
-song_name=`echo $SONG_URL | sed -e 's/.*bai-hat\/\(.*\)\/.*/\1/'`
-song_xml=`curl $SONG_MODE $SONG_URL 2>/dev/null | grep -oE '<embed id.*></embed>' | sed -e 's/.*xmlURL=\(.*\)\&amp;.*/\1/'`
-song_url=`curl $SONG_MODE $song_xml 2>/dev/null | grep -oE '<source>.*</source>' | sed -e 's/.*\[CDATA\[\(.*\)\]\].*/\1/'`
+song_xml_url=`curl $CURL_MODE $INPUT_URL 2>/dev/null | grep -oE '<embed id.*></embed>' | sed -e 's/.*xmlURL=\(.*\)\&amp;.*/\1/'`
+song_xml=`curl $CURL_MODE $song_xml_url 2>/dev/null`
 
-curl -o "$song_name.mp3" -L $song_url
+song_url=`echo $song_xml | grep -oE '<source>.*</source>' | sed -e 's/.*\[CDATA\[\(.*\)\]\].*/\1/'`
+song_title=`echo $song_xml | grep -oE '<title>.*</title>' | sed -e 's/.*\[CDATA\[\(.*\)\]\].*/\1/' | xargs`
+
+curl -o "$song_title.mp3" -L $song_url
