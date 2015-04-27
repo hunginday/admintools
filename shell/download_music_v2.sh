81af59f499e314dac25ee050074115baa2c8aaba
Hung Tran <hung.tran@dena.com>
2015-04-28 01:01:51 +0700

    ok

---
+++ shell/download_music_v2.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+CURL_MODE="--compressed"
+HOME_FOLDER=/hungtd/git/study/shell
+
+read_tag () { local IFS=\< ; read -d \< E ;}
+
+download_song () {
+    echo "\"$folder_name/$song_title.mp3\", $song_mp3_url"
+    cd $HOME_FOLDER/$folder_name
+    wget -O "$song_title.mp3" $song_mp3_url
+}
+
+read -p "Input Zing mp3 URL: " input_url
+
+xml_url=`curl $CURL_MODE $input_url 2>/dev/null | grep -o '<embed id.*>' | grep -o 'http://mp3[^&]*'`
+xml_content=`curl $CURL_MODE $xml_url 2>/dev/null`
+
+if [[ "$input_url" =~ http://.*/.*/(.*)/.* ]]; then
+    folder_name=${BASH_REMATCH[1]}
+else
+    echo "Wrong input format!"
+    exit
+fi
+
+cd $HOME_FOLDER
+mkdir $folder_name
+
+
