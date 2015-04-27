271ddd9ac704a50b731e1e7e7419292777202a1f
Hung Tran <hung.tran@dena.com>
2015-04-27 16:13:51 +0700

    ok

--- shell/download_album.sh
+++ shell/download_album.sh
@@ -1,32 +1,44 @@
 #!/bin/sh
 
-
-read_dom () {
-    local IFS=\>
-    read -d \< ENTITY CONTENT
-    local RET=$?
-    TAG_NAME=${ENTITY%% *}
-    ATTRIBUTES=${ENTITY#* }
-    return $RET
-}
-
-parse_dom () {
-    if [[ $TAG_NAME = "item" ]] ; then
-        eval local $ATTRIBUTES
-        echo $CONTENT
-        echo $ENTITY
-        echo "foo size is: $type"
-    fi
-}
-
 INPUT_URL="http://mp3.zing.vn/album/Ke-O-Mien-Xa-Quang-Le/ZWZAIFOI.html"
 CURL_MODE="--compressed"
+HOME_FOLDER=/hungtd/git/study/shell
 
 xml_url=`curl $CURL_MODE $INPUT_URL 2>/dev/null | grep -oE '<embed id.*></embed>' | sed -e 's/.*xmlURL=\(.*\)\&amp;.*/\1/'`
 xml_content=`curl $CURL_MODE $xml_url 2>/dev/null`
 
-echo $xml_content | while read_dom; do
-    parse_dom
-done  
+rtag () { local IFS=\< ; read -d \< E ;}
 
-# echo $xml_content | awk -F '[<>]' 
+download_song () {
+    full_path="\"$album_name/$song_title.mp3\""
+    echo "$full_path, $song_mp3_url"
+    cd $HOME_FOLDER/$album_name
+    wget -O "$song_title.mp3" $song_mp3_url
+}
+
+album_name=`echo $INPUT_URL | sed -e 's/.*album\/\(.*\)\/.*/\1/'`
+cd $HOME_FOLDER
+mkdir $album_name
+
+prev_tag=""
+
+while rtag; do
+    if [[ "$prev_tag" =~ ^title\> ]]; then
+        current_title="$E"
+    fi
+
+    if [[ "$prev_tag" =~ ^source\> ]]; then
+        current_source="$E"
+        
+        song_title=`echo $current_title | sed -e 's/.*\[CDATA\[\(.*\)\]\].*/\1/' | xargs`
+        song_mp3_url=`echo $current_source | sed -e 's/.*\[CDATA\[\(.*\)\]\].*/\1/' | xargs`
+
+        download_song
+
+        title_content=""
+        source_content=""
+    fi
+
+    prev_tag="$E"
+done <<< $xml_content
+
