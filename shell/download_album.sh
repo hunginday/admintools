1b6baf4bc5a6dc6eaf0fd0354b069fb47d74ae69
Hung Tran <hung.tran@dena.com>
2015-04-27 08:55:56 +0700

    ok

--- shell/download_album.sh
+++ shell/download_album.sh
@@ -1,8 +1,32 @@
 #!/bin/sh
 
+
+read_dom () {
+    local IFS=\>
+    read -d \< ENTITY CONTENT
+    local RET=$?
+    TAG_NAME=${ENTITY%% *}
+    ATTRIBUTES=${ENTITY#* }
+    return $RET
+}
+
+parse_dom () {
+    if [[ $TAG_NAME = "item" ]] ; then
+        eval local $ATTRIBUTES
+        echo $CONTENT
+        echo $ENTITY
+        echo "foo size is: $type"
+    fi
+}
+
 INPUT_URL="http://mp3.zing.vn/album/Ke-O-Mien-Xa-Quang-Le/ZWZAIFOI.html"
 CURL_MODE="--compressed"
 
-album_xml_url=`curl $CURL_MODE $INPUT_URL 2>/dev/null | grep -oE '<embed id.*></embed>' | sed -e 's/.*xmlURL=\(.*\)\&amp;.*/\1/'`
+xml_url=`curl $CURL_MODE $INPUT_URL 2>/dev/null | grep -oE '<embed id.*></embed>' | sed -e 's/.*xmlURL=\(.*\)\&amp;.*/\1/'`
+xml_content=`curl $CURL_MODE $xml_url 2>/dev/null`
 
-echo $album_xml_url
+echo $xml_content | while read_dom; do
+    parse_dom
+done  
+
+# echo $xml_content | awk -F '[<>]' 
--- shell/download_song.sh
+++ shell/download_song.sh
@@ -3,10 +3,10 @@
 INPUT_URL="http://mp3.zing.vn/bai-hat/Dat-Viet-Buc-Tuong/ZW6FODWZ.html"
 CURL_MODE="--compressed"
 
-song_xml_url=`curl $CURL_MODE $INPUT_URL 2>/dev/null | grep -oE '<embed id.*></embed>' | sed -e 's/.*xmlURL=\(.*\)\&amp;.*/\1/'`
-song_xml=`curl $CURL_MODE $song_xml_url 2>/dev/null`
+xml_url=`curl $CURL_MODE $INPUT_URL 2>/dev/null | grep -oE '<embed id.*></embed>' | sed -e 's/.*xmlURL=\(.*\)\&amp;.*/\1/'`
+xml_content=`curl $CURL_MODE $xml_url 2>/dev/null`
 
-song_url=`echo $song_xml | grep -oE '<source>.*</source>' | sed -e 's/.*\[CDATA\[\(.*\)\]\].*/\1/'`
-song_title=`echo $song_xml | grep -oE '<title>.*</title>' | sed -e 's/.*\[CDATA\[\(.*\)\]\].*/\1/' | xargs`
+song_mp3_url=`echo $xml_content | grep -oE '<source>.*</source>' | sed -e 's/.*\[CDATA\[\(.*\)\]\].*/\1/'`
+song_title=`echo $xml_content | grep -oE '<title>.*</title>' | sed -e 's/.*\[CDATA\[\(.*\)\]\].*/\1/' | xargs`
 
-curl -o "$song_title.mp3" -L $song_url
+curl -o "$song_title.mp3" -L $song_mp3_url
