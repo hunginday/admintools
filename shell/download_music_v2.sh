718aa14bbc1fc7a8c55f31fab3ddb6c921961161
Hung Tran <hung.tran@dena.com>
2015-04-29 01:10:35 +0700

    ok

--- shell/download_music_v2.sh
+++ shell/download_music_v2.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
+
+OUT_FOLDER=/hungtd/git/study/shell
 CURL_MODE="--compressed"
-HOME_FOLDER=/hungtd/git/study/shell
 VERSION=0.9
 HELP="""Usage: $0 [OPTION] [MP3_ZING_URL]
-
 Download a song or album from mp3.zing.vn
 
 Option:
@@ -24,12 +24,12 @@ read_tag () { local IFS=\< ; read -d \< E ;}
 
 download_song () {
     echo "\"$folder_name/$song_title.mp3\", $song_mp3_url"
-    cd $HOME_FOLDER/$folder_name
-    wget -O "$song_title.mp3" $song_mp3_url
+    cd $OUT_FOLDER/$folder_name
+    wget -qO "$song_title.mp3" $song_mp3_url
 }
 
 download_album () {
-    cd $HOME_FOLDER
+    cd $OUT_FOLDER
     mkdir -p $folder_name
 
     prev_tag=""
