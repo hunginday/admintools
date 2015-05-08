c0c4631bdf05a4ee4c44a7896087cb827359ff43
Hung Tran <hung.tran@dena.com>
2015-05-08 11:18:37 +0700

    ok

--- perl/ref/find.pl
+++ perl/ref/find.pl
@@ -8,7 +8,7 @@ use v5.10;
 
 use File::Find;
 
-my @starting_directories = ('/hungtd/git/study/');
+my @starting_directories = ('.');
 
 find(
     sub {
@@ -16,3 +16,24 @@ find(
     },
     @starting_directories,
 );
+
+my $total_size1 = 0;
+find(sub { $total_size1 += -s if -f }, '.');
+print "total_size1: ", $total_size1, "\n";
+
+sub create_find_callback_that_sums_the_size {
+    my $total_size = 0;
+        return sub {
+            if (@_) { # it's our dummy invocation
+            return $total_size;
+        } else { # it's a callback from File::Find:
+            $total_size += -s if -f;
+        }
+    };
+}
+
+my $callback = create_find_callback_that_sums_the_size( );
+find($callback, '.');
+my $total_size = $callback->('dummy'); # dummy parameter to get size
+print "total size of bin is $total_size\n";
+
