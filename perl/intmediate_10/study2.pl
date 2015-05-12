21060e20d9e2f419ef0db30f32505fe3e05339e9
Hung Tran <hung.tran@dena.com>
2015-05-12 22:01:06 +0700

    ok

--- perl/intmediate_10/study2.pl
+++ perl/intmediate_10/study2.pl
@@ -25,18 +25,19 @@ sub data_for_path {
             else {
                 my $hash = {};
                 opendir (my $dh, $path);
-                
+
                 my @new_paths = map {
                     catfile( $path, $_ )
                 }
                 grep { ! /^\.\.?\z/ } 
                 readdir $dh;
-                
+
                 unshift @queue, map { [ $_, $hash ] } @new_paths;
                 $hash;
             }
         };
     }
+
     $data;
 }
 
