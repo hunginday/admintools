ef826da8e99f02b41a1b46ad454741a8cb453309
Hung Tran <hung.tran@dena.com>
2015-05-12 18:24:12 +0700

    ok

--- perl/intmediate_10/study2.pl
+++ perl/intmediate_10/study2.pl
@@ -16,8 +16,8 @@ sub data_for_path {
     my( $path ) = @_;
     my $data = {};
     my @queue = ( [ $path, $data ] );
-    
-    while( my $next = shift @queue ) {
+
+    while ( my $next = shift @queue ) {
         my( $path, $ref ) = @$next;
         my $basename = basename( $path );
         $ref->{$basename} = do {
@@ -25,9 +25,13 @@ sub data_for_path {
             else {
                 my $hash = {};
                 opendir (my $dh, $path);
+                
                 my @new_paths = map {
-                catfile( $path, $_ )
-                } grep { ! /^\.\.?\z/ } readdir $dh;
+                    catfile( $path, $_ )
+                }
+                grep { ! /^\.\.?\z/ } 
+                readdir $dh;
+                
                 unshift @queue, map { [ $_, $hash ] } @new_paths;
                 $hash;
             }
