3317ba425068a2e7046f0c0df7b6d4ca10687a23
Hung Tran <hung.tran@dena.com>
2015-05-13 17:47:33 +0700

    ok

---
+++ perl/intmediate_11/Navigation.pm
@@ -0,0 +1,31 @@
+package Navigation;
+
+use strict;
+use warnings;
+use Data::Dumper;
+use Data::Dump qw(dump); # CPAN module
+use v5.14;
+
+our @homeport = (21.283, -157.842);
+
+sub turn_toward_heading {
+#.. code here ..
+}
+
+sub turn_toward_port {
+#.. code here ..
+}
+
+sub get_me_home {
+    my @homeport = (1, 2);
+    say "homeport1: ";
+    dump \@homeport;
+
+    say "homeport2: ";
+    dump \@Navigation::homeport;
+}
+
+say "homeport in compiled time: ";
+dump \@homeport;
+
+1;
---
+++ perl/intmediate_11/x.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Data::Dumper;
+use Data::Dump qw(dump); # CPAN module
+use v5.14;
+
+require 'Navigation.pm';
+
+sub turn_toward_port {
+    Navigation::turn_toward_heading(compute_heading_to_island( ));
+}
+
+sub compute_heading_to_island {
+#.. code here ..
+}
+
+
+Navigation::get_me_home();
+
+my @destination = @Navigation::homeport;
+say "destination: ";
+dump \@destination;
