af2c6a89ddf3280fb5689c0dd6a24ccaf693f3c4
Hung Tran <hung.tran@dena.com>
2015-05-15 10:26:34 +0700

    ok

---
+++ perl/seminar_intermediate/name_sort.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Data::Dumper;
+use Data::Dump qw(dump); # CPAN module
+
+use v5.14;
+use Benchmark;
+
+my @unsorted;
+my @sorted1;
+my @sorted2;
+
+while (<>) {
+    next if /^#/;
+    chomp;
+    push @unsorted, $_;
+}
+
+@unsorted = ("Beckie Silvestrini A",
+  "Joesph Degonia A");
+
+timethese( 0, {
+    "schwartzian" => sub { @sorted1 = 
+                                map{ $_->[0] }
+                                sort { $a->[1] cmp $b->[1] }
+                                map { m/(.*?)\s*(\S+)$/; [$_, "$2 $1" ] }
+                                @unsorted },
+    "sort routine" => sub { @sorted2 = sort mysort @unsorted }
+});
+
+dump \@sorted1;
+
+sub mysort
+{
+    $a =~ m/(.*?)\s*(\S+)$/;
+    my $aa = "$2 $1";
+    $b =~ m/(.*?)\s*(\S+)$/;
+    my $bb = "$2 $1";
+    return $aa cmp $bb;
+}
