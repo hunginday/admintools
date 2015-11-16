bd4c591f4c58078146f3bb7c481c7d3c83f88917
Hung Tran <hung.tran@dena.com>
2015-11-16 14:27:02 +0700

    ok

---
+++ shell/lpic/echo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+var1=$1
+var2=$2
+var3=$3
+
+echo here is a complex var1=$var1 var2=$var2
+echo 'try another, "var1('$var1')", var2('$var2')."'
+echo "this is anormal var1($var1), var2($var2)."
+echo "this is even more complex ('$var1', '$var2')"
+echo 'this is even more more complex ("$var1", "$var2")'
+
